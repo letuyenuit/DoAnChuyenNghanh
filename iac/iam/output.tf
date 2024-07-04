b8c72d2b9577990ceb732a5dbc60949ab13ae55c
tuyendepzai <letuyenkhtn212@gmail.com>
2024-07-04 14:46:49 +0700

    test rds

--- iac/iam/output.tf
+++ iac/iam/output.tf
@@ -1,7 +1,3 @@
 output "role_arn" {
   value = aws_iam_role.ecs-task-execution-role-ecs-fargate.arn
 }
-
-output "RDSCustomSQLServerInstanceProfileRole" {
-  value = aws_iam_role.RDSCustomSQLServerInstanceProfileRole.arn
-}
