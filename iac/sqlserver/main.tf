b4e49a1535cbe48f647d55b2ab015541211148f9
tuyendepzai <letuyenkhtn212@gmail.com>
2024-07-04 21:57:27 +0700

    edit snapshot

--- iac/sqlserver/main.tf
+++ iac/sqlserver/main.tf
@@ -21,18 +21,19 @@ resource "aws_security_group" "rds_mssql_security_group" {
 }
 
 resource "aws_db_instance" "default_mssql" {
-  identifier             = "sqlserver-chat-db"
-  allocated_storage      = 20
-  storage_type           = "gp3"
-  engine                 = "sqlserver-ex"
-  engine_version         = "15.00.4375.4.v1"
-  instance_class         = "db.t3.micro"
-  multi_az               = false
-  username               = "chatdb"
-  password               = "admin1234"
-  vpc_security_group_ids = ["${aws_security_group.rds_mssql_security_group.id}"]
-  db_subnet_group_name   = aws_db_subnet_group.default_rds_mssql.id
-  skip_final_snapshot    = true
+  identifier                = "sqlserver-chat-db"
+  allocated_storage         = 20
+  storage_type              = "gp3"
+  engine                    = "sqlserver-ex"
+  engine_version            = "15.00.4375.4.v1"
+  instance_class            = "db.t3.micro"
+  multi_az                  = false
+  username                  = "chatdb"
+  password                  = "admin1234"
+  vpc_security_group_ids    = ["${aws_security_group.rds_mssql_security_group.id}"]
+  db_subnet_group_name      = aws_db_subnet_group.default_rds_mssql.id
+  skip_final_snapshot       = false
+  final_snapshot_identifier = "final-snapshot-identifier"
 }
 
 
